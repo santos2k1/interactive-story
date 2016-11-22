3a2a8090e492957cb40749864a905c670e73ebd8
Michael <michaelsantos@michaels-macbook-pro.local>
2016-11-22 09:33:39 -0600

    Initial Commit

---
+++ InteractiveStory/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by Pasan Premaratne on 3/3/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "startAdventure" {
+            if let pageController = segue.destinationViewController as? PageController {
+                pageController.page = Adventure.story
+            }
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
